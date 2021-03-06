ac37121951337c78243f15c09302a72655cfb862
shaarifsabah <shaarifsabah5299@gmail.com>
2021-03-06 18:46:31 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './index.css';
+import Header from './component/Header';
+
+function App() {
+
+    return (
+        <div className="Container">
+            <Header />
+        </div>
+
+    );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Panday  {2*6}</h1>
+
+        </header>
+    )
+}
+export default Header
